1b36343d7df91f1b82d390c533ccd295044f3781
Noe <noemk2@gmail.com>
2022-12-17 03:39:22 +0000

    vol function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+    // {
+    let area = area_of(width, height);
+    // }
+
+    println!("Area is {}", area);
+
+    println!("Volume is {:?}", volume(width, height, depth));
+
+    //end
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    return x * y;
+}
+fn volume(x: i32, y: i32, z: i32) -> i32 {
+    return x * y * z;
 }
