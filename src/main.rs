12e45f373b1872f7d8fa382e6719a0d76854dcc4
Noe <noemk2@gmail.com>
2022-12-17 03:25:02 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 // #[warn(unused_variables)]
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT : i32 = 2;
 
 fn main() {
-    let missiles = 8;
-    let ready = 2;
+    let mut missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = ready - missiles;
+    println!("{} missiles left", missiles);
 }
