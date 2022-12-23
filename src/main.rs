6b22c480667aeed3e0fb36e7f3c030a4dcaeb262
Noe <noemk2@gmail.com>
2022-12-23 01:17:27 +0000

    Complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Silence some warnings so they don't distract from the exercise.
-#![allow(unused_mut, unused_variables)]
+#![allow(unused_mut, unused_variables, dead_code)]
 // use std::fmt::Write;
 
 fn main() {
@@ -24,19 +24,24 @@ fn main() {
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    change(&mut arg);
-    println!("I have many {}", arg);
+    // change(&mut arg);
+    // println!("I have many {}", arg);
 
+    // --
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
     // indicating whether or not the String both starts with a "b" AND contains an "a".
     // Hint 1: use `.starts_with("b")` and `.contains("a")`
     // Hint 2: `&&` is the boolean "AND" operator
     //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+    if eat(arg) {
+        println!("Might be bananas");
+    } else {
+        println!("Not bananas");
+    }
+
+    fn eat(arg: String) -> bool {
+        arg.starts_with("b") && arg.contains("a")
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
@@ -47,20 +52,26 @@ fn main() {
     // Hint: You will need to dereference the mutable reference in order to assign it a
     // new value.
     //
-    // let mut material = "mud".to_string();
-    // println!("This material is just `{}`.", material);
-    // bedazzle(&mut material);
-    // println!("Wow! Now the material is `{}`!", material);
+    let mut material = "mud".to_string();
+    println!("This material is just `{}`.", material);
+    bedazzle(&mut material);
+    println!("Wow! Now the material is `{}`!", material);
+}
+
+fn bedazzle(material: &mut String) {
+    *material = "sparkly".to_string();
+}
+
+fn inspect(arg: &String) {
+    if arg.ends_with("s") {
+        println!("{} is plural", arg);
+    } else {
+        println!("{} is singular", arg)
+    }
 }
 
 fn change(arg: &mut String) {
-    arg.push_str("s");
+    if !arg.ends_with("s") {
+        arg.push_str("s")
+    }
 }
-
-// fn inspect(arg: &str) {
-//     if !arg.ends_with("s") {
-//         println!("{}", arg.to_owned() + "s");
-//     } else {
-//         println!("{}", &arg[..arg.len() - 1]);
-//     }
-// }
